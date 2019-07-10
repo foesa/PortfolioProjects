93c67bebc72b34fc91079b9c74eae2dc36ec1a52
foesa <leguavoen@gmail.com>
2019-07-10 20:57:04 +0100

    Initial commit

---
+++ PortfolioProjects/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PortfolioProjects project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PortfolioProjects.settings')
+
+application = get_wsgi_application()
