81ca098ac9514a0a8804945a966c1e57ab778cc7
foesa <leguavoen@gmail.com>
2019-07-28 21:49:13 +0100

    database for titanic done

---
+++ titanic/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    passengerId = models.IntegerField()
+    passengerClass = models.IntegerField()
+    sex = models.CharField(max_length=100)
+    age = models.FloatField()
+    ticket = models.CharField(max_length=100)
+    fare = models.FloatField()
+    cabin = models.CharField(max_length=100)
