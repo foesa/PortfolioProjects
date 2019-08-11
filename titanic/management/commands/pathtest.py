ba0f311e5f0ea9223354f91d340e815b8f49f1be
foesa <leguavoen@gmail.com>
2019-08-11 23:08:21 +0100

    Database finally totally created. Now need to create page to display persons

--- titanic/management/commands/pathtest.py
+++ titanic/management/commands/pathtest.py
@@ -1,33 +1,59 @@
 import os
 import csv
+import pandas as pd
+import numpy as np
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from titanic.models import Person
 
 
+def Agefill(database):
+    Age = database[0]
+    Pclass = database[1]
+
+    if pd.isnull(Age):
+        if Pclass == 1:
+            return 37
+        elif Pclass == 2:
+            return 29
+        else:
+            return 24
+    else:
+        return Age
+
+def farefill(database):
+    Fare = database[0]
+    Pclass = database[1]
+
+    if pd.isnull(Fare):
+        if Pclass == 1:
+            return 60
+        elif Pclass == 2:
+            return 25
+        elif Pclass == 3:
+            return 10
+    else:
+            return Fare
+
+# TODO Maybe put into one function to prevent double iteration through the file.
 class Command(BaseCommand):
     help = 'Builds database from CSV file'
 
 
     def handle(self, *args, **options):
         path = "/home/foesa/Documents/Refactored_Py_DS_ML_Bootcamp-master/13-Logistic-Regression/titanic_test.csv"
-        count = True
-        with open(path) as f:
-            has_header = csv.Sniffer().has_header(f.read(1024))
-            f.seek(0)
-            reader = csv.reader(f)
-            if has_header:
-                next(reader)
-            for row in reader:
-                print(" ".join(row))
+        database = pd.read_csv(path)
+        database['Age'] = database[['Age', 'Pclass']].apply(Agefill, axis=1)
+        database['Fare'] = database[['Fare','Pclass']].apply(farefill,axis=1)
+        for row in database.itertuples(index = True, name = "Pandas"):
                 _, created = Person.objects.get_or_create(
-                        name=row[2],
-                        passengerId=row[0],
-                        passengerClass=row[1],
-                        sex=row[3],
-                        age=row[4],
-                        ticket=row[7],
-                        fare=row[8],
-                    )
+                    name=getattr(row, 'Name'),
+                    passengerId=getattr(row, 'PassengerId'),
+                    passengerClass=getattr(row, 'Pclass'),
+                    sex=getattr(row, 'Sex'),
+                    age=getattr(row, 'Age'),
+                    ticket=getattr(row, 'Ticket'),
+                    fare=getattr(row, 'Fare'),
+                )
 
 
-#TODO Missing values in file need to fill these in somwhow to prevent errors in creating Database
